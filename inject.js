c9bac034e14474495008b2077ec2794b2c608142
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-08-07 09:41:39 +0900

    V1.9.7 (#30)
    * revert
    
    * wakeUpControls
    
    * local pin
    
    ---------
    
    Co-authored-by: Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>

---
+++ inject.js
@@ -0,0 +1,24 @@
+(() => {
+	let player;
+	let pin_interval;
+
+	const detect_interval = setInterval(() => {
+		player = document.body.querySelector('div#movie_player');
+		if (!player) {
+			return;
+		}
+
+		clearInterval(detect_interval);
+
+		document.addEventListener('_pin_bottom_update', e => {
+			clearInterval(pin_interval);
+			if (e.detail) {
+				pin_interval = setInterval(() => {
+					player.wakeUpControls();
+				}, 1000);
+			}
+		});
+
+		document.dispatchEvent(new CustomEvent('_pin_bottom_init'));
+	});
+})();
