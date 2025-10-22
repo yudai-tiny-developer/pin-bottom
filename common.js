95ee8c3c400137130dd953d8b249fefbc6bd4fd6
Simple and Tiny Utilities <108601122+yudai-tiny-developer@users.noreply.github.com>
2025-10-22 23:59:25 +0900

    v1.10.0 (#33)
    Hide overlay in fullscreen mode
    
    Co-authored-by: Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>

--- common.js
+++ common.js
@@ -1,10 +1,12 @@
 export const storage = [
     'pin',
     'space',
+    'overlays',
 ];
 
 export const default_pin = false;
 export const default_space = false;
+export const default_overlays = false;
 
 export function value(value, defaultValue) {
     return value === undefined ? defaultValue : value;
