cec51de516a1ebbad9448aa8ed5e43f7a6b32bf2
Simple and Tiny Utilities <yudai-tiny-developer@gmail.com>
2026-03-28 00:18:26 +0900

    v1.11.4

--- content.js
+++ content.js
@@ -5,83 +5,30 @@ import(chrome.runtime.getURL('common.js')).then(common => {
 });
 
 function main(common) {
-    function loadSettings(skip) {
+    function loadSettings(initializePin = true) {
         chrome.storage.local.get(common.storage, data => {
             const initial_pin = common.value(data.pin, common.default_pin);
             const space = common.value(data.space, common.default_space);
             const overlays = common.value(data.overlays, common.default_overlays);
 
-            if (!skip) {
-                update_button(initial_pin);
-            }
+            if (initializePin) initial_pin ? on() : off();
 
-            clearInterval(space_interval);
-            space_interval = setInterval(() => {
-                if (space && current_pin) {
-                    const video = video_instance();
-                    if (!video.style.height.startsWith('calc') && area.offsetHeight > 0) {
-                        prev_left = video.style.left;
-                        prev_width = video.style.width;
-                        prev_height = video.style.height;
-
-                        let offsetHeight;
-                        if (video.hasAttribute('playsinline')) { // new-style YouTube embedded player
-                            offsetHeight = area.offsetHeight * 2;
-                        } else {
-                            offsetHeight = panel.offsetHeight;
-                        }
-
-                        const space_height = Math.min(Math.max(offsetHeight - (player.offsetHeight - video.offsetHeight) / 2.0, 0), offsetHeight);
-                        const new_height = video.offsetHeight - space_height;
-                        const new_width = video.offsetWidth * new_height / video.offsetHeight;
-                        const new_left = Math.max((player.offsetWidth - new_width) / 2.0, 0);
-
-                        video.style.left = `calc(${new_left}px)`;
-                        video.style.width = `calc(${new_width}px)`;
-                        video.style.height = `calc(${new_height}px)`;
-                    }
-
-                    player?.classList.add('_pin_bottom_button_space');
-                } else {
-                    if (prev_height) {
-                        const video = video_instance();
-
-                        video.style.left = prev_left;
-                        video.style.width = prev_width;
-                        video.style.height = prev_height;
-
-                        prev_left = undefined;
-                        prev_width = undefined;
-                        prev_height = undefined;
-                    }
-
-                    player?.classList.remove('_pin_bottom_button_space');
-                }
-            }, 250);
-
-            if (overlays) {
-                player?.classList.add('_pin_bottom_hide_overlays');
-            } else {
-                player?.classList.remove('_pin_bottom_hide_overlays');
-            }
+            update_space(space);
+            update_overlays(overlays);
         });
-    }
-
-    function update_button(pin) {
-        pin ?? common.default_pin ? on() : off();
     }
 
     function on() {
         current_pin = true;
-        player?.classList.add('_pin_bottom_button_on');
-        pin_button?.classList.add('_pin_bottom_button_on');
+        player.classList.add('_pin_bottom_button_on');
+        pin_button.classList.add('_pin_bottom_button_on');
         document.dispatchEvent(new CustomEvent('_pin_bottom_update', { detail: current_pin }));
     }
 
     function off() {
         current_pin = false;
-        player?.classList.remove('_pin_bottom_button_on');
-        pin_button?.classList.remove('_pin_bottom_button_on');
+        player.classList.remove('_pin_bottom_button_on');
+        pin_button.classList.remove('_pin_bottom_button_on');
         document.dispatchEvent(new CustomEvent('_pin_bottom_update', { detail: current_pin }));
     }
 
@@ -93,72 +40,123 @@ function main(common) {
         return button;
     }
 
-    function append_button() {
-        if (pin_button) {
-            const action_menu = document.getElementsByTagName('player-fullscreen-action-menu')?.[0];
-            if (action_menu) { // new-style YouTube embedded player
-                pin_button.style.width = '40px';
-                pin_button.style.height = '40px';
+    function update_button() {
+        clearInterval(append_button_interval);
+        append_button_interval = setInterval(() => {
+            if (common.isEmbed(location.href)) {
+                if (pin_button.style.width !== '40px') {
+                    Object.assign(pin_button.style, {
+                        width: '40px',
+                        height: '40px',
+                    });
+                }
+
+                const container = document.getElementsByTagName('player-fullscreen-action-menu')[0]?.querySelector('div.quick-actions-wrapper');
+                if (container && !container.contains(pin_button)) {
+                    container.appendChild(pin_button);
+                    area = container;
+                }
             } else {
-                pin_button.style.width = undefined;
-                pin_button.style.height = undefined;
+                if (pin_button.style.width !== undefined) {
+                    Object.assign(pin_button.style, {
+                        width: undefined,
+                        height: undefined,
+                    });
+                }
+
+                const container = player.querySelector('div.ytp-right-controls-right');
+                if (container && !container.contains(pin_button)) {
+                    container.appendChild(pin_button);
+                    area = player.querySelector('div.ytp-chrome-bottom');
+                }
             }
-            area.appendChild(pin_button);
+        }, 500);
+    }
+
+    function update_space(space) {
+        clearInterval(space_interval);
+        space_interval = setInterval(() => {
+            if (space && current_pin) {
+                if (!area) return;
+                if (area.offsetHeight === 0) return;
+
+                const video = player.querySelector('video.html5-main-video');
+                if (!video) return;
+                if (video.style.height.startsWith('calc')) return;
+
+                prev_left = video.style.left;
+                prev_width = video.style.width;
+                prev_height = video.style.height;
+
+                const offsetHeight = common.isEmbed(location.href) ? area.offsetHeight * 2 : area.offsetHeight;
+                const space_height = Math.min(Math.max(offsetHeight - (player.offsetHeight - video.offsetHeight) / 2.0, 0), offsetHeight);
+                const new_height = video.offsetHeight - space_height;
+                const new_width = video.offsetWidth * new_height / video.offsetHeight;
+                const new_left = Math.max((player.offsetWidth - new_width) / 2.0, 0);
+
+                Object.assign(video.style, {
+                    left: `calc(${new_left}px)`,
+                    width: `calc(${new_width}px)`,
+                    height: `calc(${new_height}px)`,
+                });
+
+                player.classList.add('_pin_bottom_button_space');
+            } else {
+                if (!prev_height) return;
+
+                const video = player.querySelector('video.html5-main-video');
+                if (!video) return;
+
+                Object.assign(video.style, {
+                    left: prev_left,
+                    width: prev_width,
+                    height: prev_height,
+                });
+
+                prev_left = undefined;
+                prev_width = undefined;
+                prev_height = undefined;
+
+                player.classList.remove('_pin_bottom_button_space');
+            }
+        }, 250);
+    }
+
+    function update_overlays(overlays) {
+        if (overlays) {
+            player.classList.add('_pin_bottom_hide_overlays');
+        } else {
+            player.classList.remove('_pin_bottom_hide_overlays');
         }
     }
 
-    function video_instance() {
-        if (!video?.parentNode && player) {
-            video = player.querySelector('video.html5-main-video');
-        }
-        return video;
-    }
-
-    const shortcut_command = () => {
+    function shortcut_command() {
         current_pin ? off() : on();
         chrome.storage.local.set({ pin: current_pin });
-    };
+    }
+
+    const pin_button = create_button();
 
     let player;
-    let video;
-    let panel;
-    let pin_button;
+    let area;
     let current_pin;
     let prev_left;
     let prev_width;
     let prev_height;
     let space_interval;
-
-    chrome.runtime.onMessage.addListener(shortcut_command);
+    let append_button_interval;
 
     document.addEventListener('_pin_bottom_init', () => {
         const detect_interval = setInterval(() => {
             player = document.getElementById("movie_player");
             if (!player) return;
 
-            const action_menu = document.getElementsByTagName('player-fullscreen-action-menu')?.[0];
-            if (action_menu) { // new-style YouTube embedded player
-                area = action_menu.querySelector('div.quick-actions-wrapper');
-                if (!area) return;
-
-                panel = action_menu.querySelector('div.fullscreen-watch-next-entrypoint-wrapper');
-                if (!panel) return;
-            } else {
-                area = player.querySelector('div.ytp-right-controls-left');
-                if (!area) return;
-
-                panel = player.querySelector('div.ytp-chrome-bottom');
-                if (!panel) return;
-            }
-
             clearInterval(detect_interval);
 
-            pin_button = create_button();
-            append_button();
-
-            chrome.storage.onChanged.addListener(() => loadSettings(true));
-
-            loadSettings();
+            update_button();
+            chrome.runtime.onMessage.addListener(shortcut_command);
+            chrome.storage.onChanged.addListener(() => loadSettings(false));
+            loadSettings(true);
         }, 500);
     });
 
